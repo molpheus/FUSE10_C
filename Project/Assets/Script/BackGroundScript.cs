79d71bceb027be74696fc59068686e35c9b2de1f
佐藤 千里 <preventer.ark.wind@gmail.com>
2013-11-30 15:15:27 +0900

    Add GameScene

---
+++ Project/Assets/Script/BackGroundScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundScript : MonoBehaviour {
+
+	// 背景の移動スピード
+	public static float spd = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 背景をX軸方向に移動
+		transform.position += new Vector3(spd, 0, 0);
+
+		// 背景が指定位置まで来たら削除
+		if(transform.position.x >= 40) {
+			transform.position = new Vector3(-7.5f,5,5);
+		}
+	}
+}
---
+++ Project/Assets/Script/FloorScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorScript : MonoBehaviour {
+
+	// 床の移動スピード
+	public static float spd = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 床をX軸方向に移動
+		transform.position += new Vector3(spd, 0, 0);
+
+		// 床が指定位置まで来たら削除
+		if(transform.position.x >= 40) {
+			transform.position = new Vector3(0,0,0);
+		}
+	}
+}

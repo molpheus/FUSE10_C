94190d28334651abb25dc7794f61741ad80910d0
molpheus <tudoinoizumi@yahoo.co.jp>
2013-12-01 23:17:35 +0900

    no message

--- Project/Assets/Script/CharaCtrl.cs
+++ Project/Assets/Script/CharaCtrl.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CharaCtrl : MonoBehaviour
 {
-
+	public bool jump = false;
     // Use this for initialization
     void Start()
     {
@@ -13,7 +13,7 @@ public class CharaCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if(Input.GetKeyDown(KeyCode.A))
+		if(Input.GetKeyDown(KeyCode.A) && !jump)
 		{
 			Jumps();
 			SendMessage("Jump");
@@ -23,6 +23,7 @@ public class CharaCtrl : MonoBehaviour
     void Jumps()
     {
 		gameObject.rigidbody2D.velocity = Vector3.up * 6f;
+		jump = true;
     }
 	IEnumerator JumpMove()
 	{
@@ -40,5 +41,42 @@ public class CharaCtrl : MonoBehaviour
 			GameUtility.speed -= 1f;
 		}
 		GUI.Label(new Rect(100, 0, 50, 50), GameUtility.speed.ToString());
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log("other.gameObject.tag : " + other.gameObject.tag);
+		if (other.gameObject.tag == "floor")
+		{
+			jump = false;
+		}
+
+		if (other.gameObject.tag == "ball")
+		{
+			if (jump)
+			{
+				if (transform.position.y > other.gameObject.transform.position.y)
+				{
+					Object.Destroy(other.gameObject);
+				}
+				else
+				{
+					gameObject.rigidbody2D.fixedAngle = false;
+					gameObject.rigidbody2D.isKinematic = true;
+					gameObject.rigidbody2D.velocity = Vector3.up * 10f + Vector3.right * 10f;
+				}
+			}
+			else
+			{
+				gameObject.rigidbody2D.fixedAngle = false;
+				gameObject.rigidbody2D.isKinematic = true;
+				gameObject.rigidbody2D.velocity = Vector3.up * 10f + Vector3.right * 10f;
+			}
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D other)
+	{
+		jump = true;
 	}
 }

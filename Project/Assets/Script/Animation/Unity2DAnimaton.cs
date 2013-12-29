6140705127f08c009a04ec11735e5326a842ac89
zerodivS <zerodiv.S.00@gmail.com>
2013-12-30 03:19:14 +0900

    no message

---
+++ Project/Assets/Script/Animation/Unity2DAnimaton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unity2DAnimaton : MonoBehaviour {
+	public Sprite[] sprite;
+
+	bool flg = false;
+
+	int cnt = 1;
+
+	int num = 0;
+
+	int number = 0;
+
+	public SpriteRenderer render;
+
+	void Start()
+	{
+		if (sprite.Length == 0) {
+						flg = true;
+				} else {
+			render.sprite = sprite[number];
+				}
+
+	}
+
+	void Update()
+	{
+		if (flg) {
+						return;
+				}
+
+		if (cnt < num) {
+			number++;
+			  			if(number >= sprite.Length)
+			{
+				number = 0;
+			}
+			render.sprite = sprite[number];
+			num = 0;
+				}
+		num++;
+	}
+}
